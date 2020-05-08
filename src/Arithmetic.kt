a07b1546e46fef21a57a759aec1c37d83a0705ae
rexfilius <osakweifykay@gmail.com>
2020-05-08 05:18:19 +0100

    first commit

---
+++ src/Arithmetic.kt
@@ -0,0 +1,24 @@
+class Arithmetic(private val x: Int, private val y: Int) {
+
+    fun add() {
+        val result = x + y
+        println("$x plus $y = $result")
+    }
+
+    fun subtract() {
+        val result = x - y
+        println("$x minus $y = $result")
+    }
+
+    fun multiply() {
+        val result = x * y
+        println("$x times $y = $result")
+    }
+}
+
+fun main() {
+    val arithmetic =  Arithmetic(7, 6)
+    arithmetic.add()
+    arithmetic.subtract()
+    arithmetic.multiply()
+}
